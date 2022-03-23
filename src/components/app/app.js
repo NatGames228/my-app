7143b6a58b66b37303d5e5957e5363a3242c3d67
NatGames <salavat-iskhakov@mail.ru>
2022-03-23 20:13:47 +0300

    add key

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,9 +10,9 @@ import './app.css';
 function App() {
 
   const data = [
-    {name: 'Adel M.', salary: 5000, increase: false},
-    {name: 'Boris S.', salary: 7000, increase: false},
-    {name: 'Vladimir S.', salary: 4000, increase: true},
+    {name: 'Adel M.', salary: 5000, increase: false, id: 1},
+    {name: 'Boris S.', salary: 7000, increase: false, id: 2},
+    {name: 'Vladimir S.', salary: 4000, increase: true, id: 3},
   ]
 
   return(
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,10 @@ import './employees-list.css';
 const EmployeesList = ({data}) => {
 
   const elements = data.map(item => {
+    const {id, ...itemPromps} = item;
     return(
       // <EmployeesListItem name={item.name} salary={item.salary} />
-      <EmployeesListItem {...item} /> // equivalent
+      <EmployeesListItem key={id} {...itemPromps} /> // equivalent
     );
   });
 
